774afaadca908231bd6d5a0de38ca50d655ee8d6
Nis <nissia.sabri@gmail.com>
2014-07-04 21:15:44 +0000

    no blank address or description

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
-	  validates :name, :presence => true
+	  validates :name, :presence => true, :uniqueness => true, :length => { :maximum => 100 }
+	  validates :address, :presence => true
+	  validates :description, :presence => true
 end
