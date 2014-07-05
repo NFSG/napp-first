f9d7150f39fe523f589334095b97952e8834d9c4
Nis <nissia.sabri@gmail.com>
2014-07-05 18:00:17 +0000

    format emails sent for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,14 +1,18 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
+  after_create :send_comment_email
   RATINGS = {
-    'one star'    => '1_star',
-    'two stars'   => '2_stars',
-    'three stars' => '3_stars',
-    'four stars'  => '4_stars',
-    'five stars'  => '5_stars'
+    '1 star'    => '1_star',
+    '2 stars'   => '2_stars',
+    '3 stars' => '3_stars',
+    '4 stars'  => '4_stars',
+    '5 stars'  => '5_stars'
   }
   def humanized_rating
     RATINGS.invert[self.rating]
+  end
+  def send_comment_email
+      NotificationMailer.comment_added(self).deliver
   end
 end
