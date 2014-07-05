a57766f3ddf527c5c7b05bf7e6fbc7f28ac308fc
Nis <nissia.sabri@gmail.com>
2014-07-05 18:16:57 +0000

    send emails from server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,16 @@ Napp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
-    config.action_mailer.default_url_options = { :host => 'napp-nis.herokuapp.com' }
+    ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+  config.action_mailer.default_url_options = { :host => 'napp-nis.herokuapp.com' }
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
